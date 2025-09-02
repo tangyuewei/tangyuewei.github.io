d9b8c2ed4a74726c341063246f2f471375274dd4
tangyuewei <472680811@qq.com>
2025-09-02 02:37:24 +0000

    deploy

--- assets/js/data/swcache.js
+++ assets/js/data/swcache.js
@@ -1,43 +1,46 @@
----
-  layout: compress
-# The list to be cached by PWA
----
-
 const resource = [
   /* --- CSS --- */
-  '{{ "/assets/css/:THEME.css" | replace: ':THEME', site.theme | relative_url }}',
+  '/assets/css/jekyll-theme-chirpy.css',
 
   /* --- PWA --- */
-  '{{ "/app.js" | relative_url }}',
-  '{{ "/sw.js" | relative_url }}',
+  '/app.js',
+  '/sw.js',
 
   /* --- HTML --- */
-  '{{ "/index.html" | relative_url }}',
-  '{{ "/404.html" | relative_url }}',
+  '/index.html',
+  '/404.html',
 
-{% for tab in site.tabs %}
-'{{ tab.url | relative_url }}',
-{% endfor %}
+
+'/categories/',
+
+'/tags/',
+
+'/archives/',
+
+'/about/',
+
+'/nav/',
+
+'/zhanbu/',
+
+'/laiyigua/',
+
 
 /* --- Favicons & compressed JS --- */
-{% assign cache_list = site.static_files | where: 'swcache', true  %}
-{% for file in cache_list %}
-'{{ file.path | relative_url }}'{%- unless forloop.last -%},{%- endunless -%}
-{% endfor %}
+
+
 ];
 
 /* The request url with below domain will be cached */
 const allowedDomains = [
-  {% if site.google_analytics.id != empty and site.google_analytics.id %}
+  
 'www.googletagmanager.com',
   'www.google-analytics.com',
-{% endif %}
 
-'{{ site.url | split: "//" | last }}',
 
-{% if site.img_cdn contains '//' and site.img_cdn %}
-'{{ site.img_cdn | split: '//' | last | split: '/' | first }}',
-{% endif %}
+'tangyuewei.github.io',
+
+
 
 'fonts.gstatic.com',
   'fonts.googleapis.com',
@@ -47,3 +50,4 @@ const allowedDomains = [
 
 /* Requests that include the following path will be banned */
 const denyUrls = [];
+
